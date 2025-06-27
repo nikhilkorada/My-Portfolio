3ca6607df64d95abe8d08b2270a8aa880b47887b
Nikhil korada <nikhilkorada7@gmail.com>
2025-06-27 14:23:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Hero from './components/Hero';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+  return (
+    <main className='bg-gray-950'>
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+import {FiMenu, FiX} from 'react-icons/fi'
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const NavbarLinks =[
+        {id:1, name:"Home", link:"#home"},
+        {id:2, name:"About", link:"#about"},
+        {id:3, name:"Skills", link:"#skills"},
+        {id:4, name:"My Projects", link:"#projects"},
+    ]
+  return (
+    <header className='fixed top-0 left-0 w-full z-20 text-white' data-aos="fade-up" data-aos-delay="300">
+        <div className='container mx-auto flex items-center justify-between p-5'>
+            {/* Logo Section */}
+            <a href="#home" className='text-4xl font-bold italic text-white'>Portfolio</a>
+
+            {/* Mobile Menu Toggle Button */}
+            <button className='md:hidden focus:outline-none' onClick={()=>setIsOpen(!isOpen)}>
+                <FiMenu className='w-8 h-8 text-white' />
+            </button>
+
+            {/*Desktop Navigation Links */}
+            <nav className='hidden md:flex items-center space-x-7'>
+                {NavbarLinks.map((link) =>(
+                    <a key={link.id} href={link.link} className='hover:text-gray-200 text-lg'>
+                        {link.name}
+                    </a>
+                ))}
+                <button className='inline-flex text-white border-2 py-2 px-6 focus:outline-none hover:bg-purple-800 rounded-full text-lg'>Contact</button>
+            </nav>
+        </div>
+
+        {/* Mobile Navigation Links */}
+        <div className={`${isOpen ? 'block' : 'hidden'} md:hidden bg-[#801b9c] absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center space-y-8 pt-16`}>
+            {/* Close Button */}
+            <button className='absolute top-5 right-5 text-white' onClick={()=>setIsOpen(false)}>
+                <FiX className='w-8 h-8' />
+            </button>
+            {/* Mobile Links */}
+            {NavbarLinks.map((link) => (
+                <a key={link.id} href={link.link} className='text-lg text-white hover:text-gray-300' onClick={()=>setIsOpen(false)}>{link.name}</a>
+            ))}
+            {/* Contact Button */}
+            <button className='inline-flex text-white border-2 py-2 px-6 focus:outline-none hover:bg-purple-800 rounded-full text-lg'>Contact</button>
+        </div>
+    </header>
+  )
+}
